36f04a52876761e1c3b20fff8e99416b2ce72a2b
bayungrh <bnugraha00@gmail.com>
2022-02-21 00:54:24 +0700

    feat: add define word

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,29 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import getProps from '../utils/fetch';
+import { fetchWord, fetchDefine } from '../utils/fetch';
 
 export default async function (_: VercelRequest, res: VercelResponse) {
-  const word = await getProps();
-  res.send(`Katla hari ini adalah : <h3>${word}</h3>`);
+  const word = await fetchWord();
+  const defineWord = await fetchDefine(word);
+  const defineWordText = defineWord.join('<br/>');
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width"/>
+        <meta charSet="utf-8"/>
+      </head>
+      <body>
+        <span style="font-size: 15pt;">Katla hari ini adalah :</span><br/><br/>
+        <div style="width: 480px;">
+          <hr style="border: 1px dashed darkgrey;"/>
+          <strong style="font-size: 21pt;">${word}</strong>
+          : ${defineWordText}
+          <hr style="border: 1px dashed darkgrey;"/>
+        </div>
+        <div style="padding-top:20px;">
+          <a style="text-decoration:none;" href="https://github.com/bayungrh/katla-inspector">· Github</a>
+        </div>
+      </body>
+    </html>
+  `);
 };
--- utils/fetch.ts
+++ utils/fetch.ts
@@ -2,9 +2,15 @@ import req from 'unirest';
 import cheerio from 'cheerio';
 import decode from './codec';
 
-export default async function getProps(): Promise<string|boolean|object> {
+const BASEURI = 'https://katla.id';
+
+export async function fetchWord(): Promise<string> {
   try {
-    const katla = await req.get('https://katla.id/');
+    const katla = await req
+      .get(BASEURI)
+      .headers({
+        'referer': BASEURI
+      });
     const $ = cheerio.load(katla.body);
     let buildId: string;
     $('script').each((_, tag) => {
@@ -18,7 +24,7 @@ export default async function getProps(): Promise<string|boolean|object> {
       }
     })
     if (buildId) {
-      const getPageProps = await req.get(`https://katla.id/_next/data/${buildId}/index.json`);
+      const getPageProps = await req.get(`${BASEURI}/_next/data/${buildId}/index.json`);
       const { pageProps } = getPageProps.body;
       const { hashed } = pageProps;
       const decodeHashed = decode(hashed);
@@ -26,9 +32,23 @@ export default async function getProps(): Promise<string|boolean|object> {
       const decodeWord = decode(lastHashed);
       return decodeWord;
     }
-    return false;
+    return '';
   } catch (error) {
-    console.log('Error', error.message);
+    console.log('Error fetchWord', error.message);
+    throw error;
+  }
+}
+
+export async function fetchDefine(word: string | string[]) {
+  try {
+    const reqDefine = await req
+      .get(`${BASEURI}/api/define/${word}`)
+      .headers({
+        'referer': BASEURI
+      });
+    return reqDefine.body;
+  } catch (error) {
+    console.log('Error fetchDefine', error);
     throw error;
   }
 }
